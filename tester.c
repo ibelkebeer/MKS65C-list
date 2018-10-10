d44d9fd72932ce6486d7ee02cce9ddd687a53186
ibelkebeer <ibelkebir@stuy.edu>
2018-10-10 14:10:52 -0400

    Works on linux, not mac

--- tester.c
+++ tester.c
@@ -7,11 +7,6 @@ int main(){
   n1 -> i = 5;
   printf("Current list, 1 node with val = 5: ");
   print_list(n1);
-  /*
-  printf("Current list, 3 nodes with vals = 5, 10, and 20 respectively: ");
+  printf("Current list, 3 nodes with vals = 20, 10, and 5 respectively: ");
   print_list(insert_front(insert_front(n1, 10), 20));
-  */
-  printf("%d\n",insert_front(n1, 10) -> i);
-  printf("%d\n%d\n", n1 -> i, n1 -> next);
-  return 0;
 }
